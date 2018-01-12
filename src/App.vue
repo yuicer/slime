9a11bfc21dc4f8bec5b61abf8b765e8912fcc3c6
yuicer <yuicer@yeah.net>
2018-01-12 18:42:59 +0800

    删掉以前的逻辑，单独为slime 2d游戏

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,54 @@
 <template>
   <div id="app">
-    <transition name="fade" mode="out-in">
-    	<keep-alive exclude="start,fight">
-       		<router-view></router-view>
-		</keep-alive>
-    </transition>
-  </div>
+    <canvas id="canvas" tabindex="0" width="1000" height="800"></canvas>
+    <div id="game_intro">
+      WASD控制移动，鼠标左键控制冰锥收回与释放，注意运用背刺。
+    </div>
+	</div>
 </template>
-<script>
-	export default {
-		name: 'app',
-	}
 
+<script>
+  import game from './js/game'
+  export default {
+    name: 'fight',
+    data() {
+      return {}
+    },
+    mounted: function () {
+      game.init()
+    },
+    methods: {}
+  }
 </script>
 
 <style>
-	* {
-		-moz-user-select: none;
-		-webkit-user-select: none;
-		-ms-user-select: none;
-		user-select: none;
-	}
-	
-	button {
-		position: absolute;
-		top: 0;
-	}
-	
-	#app {
-		font-family: 'Avenir', '微软雅黑', Helvetica, Arial, sans-serif;
-	}
-	
-	body,
-	html {
-		padding: 0;
-		margin: 0;
-	}
-	
-	canvas {
-		display: block;
-	}
-	
-	.fade-enter-active,
-	.fade-leave-active {
-		transition: opacity .2s
-	}
-	
-	.fade-enter,
-	.fade-leave-active {
-		opacity: 0;
-	}
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+  }
+  #app {
+    background: #333;
+    color: white;
+    font-family: "微软雅黑";
+    font-size: 18px;
+    width: 100%;
+    height: 100%;
+  }
 
+  #canvas {
+    position: absolute;
+    left: calc(50% - 500px);
+    top: calc(50% - 400px);
+  }
+
+  #game_intro {
+    width: 550px;
+    text-align: center;
+    position: fixed;
+    left: calc(50% - 275px);
+    bottom: 10px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,100 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import VueX from 'vuex'
-
 import App from './App'
-const Start = resolve => require(['components/start'], resolve)
-const Game = resolve => require(['components/3d/main'], resolve)
-const Fight = resolve => require(['components/2d/main'], resolve)
-import text from 'src/script/text.js'
+
 Vue.config.productionTip = false
-//全局状态管理
-Vue.use(VueX)
-//全局路由
-Vue.use(VueRouter);
 
-const router = new VueRouter({
-	routes: [
-		{
-			path: '/',
-			component: Start,
-             },
-
-		{
-			path: '/game',
-			component: Game,
-        	},
-		{
-			path: '/game_fight',
-			component: Fight,
-        }
-    ],
-})
-
-const store = new VueX.Store({
-	state: {
-		//3d parameter
-		scene: new Physijs.Scene,
-		camera: {},
-		renderer: {},
-		geometry: [],
-		material: [],
-		mesh: [],
-		light: [],
-
-		//game
-		death: 0,
-		win: true,
-		lost: false,
-		mercy: 0,
-		//kaiwa
-		speed: 100,
-		text: "",
-		now_text: [],
-		kaiwa_show: false,
-		kaiwa: function (chaptor, section, speed) {
-			var me = this;
-			me.now_text = '';
-			me.kaiwa_show = true;
-			me.speed = speed;
-			me.now_text = text[chaptor][section].concat();
-			text[chaptor][section].push('');
-			me.Even(0);
-		},
-		Even: function (i) {
-			//一个even只负责一句话
-			var me = this;
-			me.text = ""
-			var d = setInterval(function () {
-				me.text += me.now_text[i].slice(0, 1);
-				me.now_text[i] = me.now_text[i].slice(1);
-				//一个字符串打完了
-				if (me.now_text[i].length < 1) {
-					//循环控制,判断有没有下一句话
-					if (me.now_text.length > i + 1)
-						setTimeout(function () {
-							me.Even(i + 1);
-						}, 1000)
-					clearInterval(d);
-					if (me.now_text.length - 1 == i)
-						setTimeout(function () {
-							me.kaiwa_show = false;
-						}, 1000)
-				}
-			}, me.speed)
-
-		},
-	},
-})
-
-export default new Vue({
-	el: '#app',
-	router,
-	store,
-	template: '<App/>',
-	components: {
-		App
-	}
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
 })
